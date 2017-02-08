12b55ffc26e0f88d23b7574d74a80f39c1ee1c32
nazuke <nazuke.hondou@googlemail.com>
2017-02-09 00:07:26 +0900

    Work in progress
    Tidied up some licensing headers.

--- MacroscopeUserControls/MacroscopePrefsControl/MacroscopePrefsControl.Designer.cs
+++ MacroscopeUserControls/MacroscopePrefsControl/MacroscopePrefsControl.Designer.cs
@@ -1,11 +1,28 @@
 ﻿/*
- * Created by SharpDevelop.
- * User: jholland
- * Date: 1/12/2017
- * Time: 18:54
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
+	
+	This file is part of SEOMacroscope.
+	
+	Copyright 2017 Jason Holland.
+	
+	The GitHub repository may be found at:
+	
+		https://github.com/nazuke/SEOMacroscope
+	
+	Foobar is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	Foobar is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 namespace SEOMacroscope
 {
 	partial class MacroscopePrefsControl
