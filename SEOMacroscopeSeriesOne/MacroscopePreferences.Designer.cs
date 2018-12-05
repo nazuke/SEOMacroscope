e82d939983c1b62bdc78b48b499f90f0d963d948
Jason Holland <nazuke.hondou@googlemail.com>
2018-12-05 15:31:42 +0900

    Updated packages and bumped version number

--- SEOMacroscopeSeriesOne/MacroscopePreferences.Designer.cs
+++ SEOMacroscopeSeriesOne/MacroscopePreferences.Designer.cs
@@ -12,7 +12,7 @@ namespace SEOMacroscope {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class MacroscopePreferences : global::System.Configuration.ApplicationSettingsBase {
         
         private static MacroscopePreferences defaultInstance = ((MacroscopePreferences)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new MacroscopePreferences())));
