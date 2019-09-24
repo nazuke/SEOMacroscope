6a0837fb38f6bac5e04e666391c8ffa79d473fd2
Jason Holland <nazuke.hondou@gmail.com>
2019-09-24 22:49:32 +0900

    Updated config and fixed licence

--- SEOMacroscopeSeriesOne/src/MacroscopeTasks/MacroscopeJobMaster.cs
+++ SEOMacroscopeSeriesOne/src/MacroscopeTasks/MacroscopeJobMaster.cs
@@ -1,25 +1,25 @@
 ﻿/*
 
-	This file is part of SEOMacroscope.
+  This file is part of SEOMacroscope.
 
-	Copyright 2019 Jason Holland.
+  Copyright 2019 Jason Holland.
 
-	The GitHub repository may be found at:
+  The GitHub repository may be found at:
 
-		https://github.com/nazuke/SEOMacroscope
+    https://github.com/nazuke/SEOMacroscope
 
-	Foobar is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+  SEOMacroscope is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-	Foobar is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+  SEOMacroscope is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with SEOMacroscope.  If not, see <http://www.gnu.org/licenses/>.
 
  */
 
